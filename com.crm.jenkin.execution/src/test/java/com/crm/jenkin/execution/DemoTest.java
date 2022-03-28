952ed91ea3c1009d114cc40cac4f0a0f2f144575
PC <PC@DESKTOP-HH2KTU2>
2022-03-28 13:02:57 +0530

    commit 1

---
+++ com.crm.jenkin.execution/src/test/java/com/crm/jenkin/execution/DemoTest.java
@@ -0,0 +1,18 @@
+package com.crm.jenkin.execution;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void method()
+	{
+		System.out.println("demo test running");
+	}
+
+	@Test
+	public void methodName()
+	{
+		System.out.println("demo1 test running");
+	}
+}
